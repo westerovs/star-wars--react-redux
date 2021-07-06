e751d15a26f0adb3339127e748c713629e57caa9
= <westerovs@mail.ru>
2021-07-06 18:41:33 +0300

    set api star wars

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,33 +1,74 @@
 import React, {Component} from 'react';
 
+import SwapiService from '../../services/swapi-service.js'
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
+    swapiService = new SwapiService()
+    
+    state = {
+        id: null,
+        name: null,
+        population: null,
+        rotationPeriod: null,
+        diameter: null
+    }
+    
+    constructor() {
+        super();
+        this.updatePlanet()
+    }
+
+    
+    updatePlanet() {
+        const id = 7
+        
+        this.swapiService
+            .getPlanet(6)
+            .then(planet => {
+                this.setState({
+                    id,
+                    name: planet.name,
+                    population: planet.population,
+                    rotationPeriod: planet.rotation_period,
+                    diameter: planet.diameter,
+                })
+            })
+    }
     
     render() {
+        const {
+            id,
+            name,
+            population,
+            rotationPeriod,
+            diameter,
+        } = this.state
+        
         return (
             <div className="random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src="https://starwars-visualguide.com/assets/img/planets/5.jpg"/>
+                     src={ `https://starwars-visualguide.com/assets/img/planets/${ id }.jpg` }
+                     alt={`planet ${ name}`}
+                />
                 <div>
-                    <h4>Planet Name</h4>
+                    <h4>{ name }</h4>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">
                             <span className="term">Population</span>
-                            <output>123124</output>
+                            <output>{ population }</output>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Rotation Period</span>
-                            <output>43</output>
+                            <output>{ rotationPeriod }</output>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Diameter</span>
-                            <output>100</output>
+                            <output>{ diameter }</output>
                         </li>
                     </ul>
                 </div>
             </div>
-        
         );
     }
 }
