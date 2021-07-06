27581393baeeb4dbdb540c0b9076a58dfe472e2d
= <westerovs@mail.ru>
2021-07-06 15:08:52 +0300

    refactoring html/css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,21 +8,21 @@ import PersonDetails from '../person-details';
 import './app.css';
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
-
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
+    return (
+        <div>
+            <Header/>
+            <RandomPlanet/>
+            
+            <div className="row mb2">
+                <div className="col-md-6">
+                    <ItemList/>
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails/>
+                </div>
+            </div>
         </div>
-        <div className="col-md-6">
-          <PersonDetails />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default App;
